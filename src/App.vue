8eaad94fcdf45e09cf40b0cd4769eac4f9e70d17
陈宇航 <1024157772@qq.com>
2022-05-05 23:39:19 +0800

    feat:新增表格模版

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import SimpleTable from "./components/table.vue";
+
+const tableColumns = [{
+  name: 'test1'
+},{
+  name: 'test2'
+},{
+  name: 'test3'
+}]
+
+const data = [{
+  test1: 'cpx',
+  test2: 'cyh',
+  test3: 'lxy'
+},{
+  test1: 'cpx',
+  test2: 'cyh',
+  test3: 'lxy'
+},{
+  test1: 'cpx',
+  test2: 'cyh',
+  test3: 'lxy'
+}]
+</script>
+
+<template>
+  <SimpleTable :columns="tableColumns" :data="data"/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px;
+}
+</style>
---
+++ src/components/table.vue
@@ -0,0 +1,33 @@
+<template>
+  <table>
+    <thead>
+      <th v-for="(headTdItem, headTdIndex) in columns" :key="`head-td-index${headTdIndex}`">
+          {{headTdItem.name}}
+      </th>
+    </thead>
+    <tbody>
+      <tr v-for="(bodyTrItem, bodyTrIndex) in data" :key="`body-tr-index${bodyTrIndex}`">
+        <td v-for="(bodyTdItem, bodyTdIndex) in columns" :key="`body-td-index${bodyTdIndex}`">
+          {{bodyTrItem[bodyTdItem.name]}}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+
+import {defineProps} from "vue";
+
+const props = defineProps({
+  columns: {
+    type: Array,
+    default: () => []
+  },
+
+  data: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
